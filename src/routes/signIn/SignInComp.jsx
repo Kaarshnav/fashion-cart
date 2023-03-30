8962bc8cbc7f511e1def94101e29a91eb7dc8703
ocarian <rajneeshupreti98@gmail.com>
2023-03-30 12:12:24 +0530

    for sign with redirect

--- src/routes/signIn/SignInComp.jsx
+++ src/routes/signIn/SignInComp.jsx
@@ -1,19 +1,38 @@
 import React from "react";
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
 
 import {
+  auth,
   signInWithGooglePopup,
   createUserDocumentFromAuth,
+  signInWithGoogleRedirect,
 } from "../../utils/firebase/firebase";
 
 const SignInComp = () => {
+  const redirectfn = async () => {
+    const response = await getRedirectResult(auth);
+    // console.log(response);
+    if (response) {
+      const userDocRef = await createUserDocumentFromAuth(response.user);
+    }
+  };
+  useEffect(() => {
+    redirectfn();
+  }, []);
   const logGogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
+    console.log(userDocRef);
   };
+
   return (
     <div>
       SignInComp
       <button onClick={logGogleUser}>signin with google popup</button>
+      <button onClick={signInWithGoogleRedirect}>
+        signin with google Redirect
+      </button>
     </div>
   );
 };
