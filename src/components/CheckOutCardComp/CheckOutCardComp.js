97d454f3f9d068c0e63b92482f99c65f3e72730e
ocarian <rajneeshupreti98@gmail.com>
2023-04-02 18:19:38 +0530

    styling added and icons  code added in html format

--- src/components/CheckOutCardComp/CheckOutCardComp.js
+++ src/components/CheckOutCardComp/CheckOutCardComp.js
@@ -1,9 +1,10 @@
 import React, { useContext } from "react";
-import "./CheckOutCardComp.styles.scss";
+// import "./CheckOutCardComp.styles.scss";
+import "./checkout-item.styles.scss";
 import { CartContext } from "../../contexts/CartContext";
 
 function CheckOutCardComp({ item }) {
-  const { id, name, price, quantity, imageUrl } = item;
+  const { name, price, quantity, imageUrl } = item;
   const { decItemsQtyToCart, incItemsQtyToCart, removeItemFromCart } =
     useContext(CartContext);
   const quantityIncHandler = () => incItemsQtyToCart(item);
@@ -12,16 +13,28 @@ function CheckOutCardComp({ item }) {
 
   return (
     <div className="checkout-item-container">
-      <img className="checkout-item-img" src={imageUrl} alt={name} />
-      <div className="checkout-item-details">
-        <h2>{name}</h2>
-        <h2>{price}</h2>
-        <button onClick={quantityIncHandler}>inc</button>
-        <h2>{quantity}</h2>
-        <button onClick={quantityDecHandler}>dec</button>
-      </div>
-      <div className="checkout-item-remove">
-        <button onClick={quantityRemoveHandler}>remove</button>
+      <img className="image-container" src={imageUrl} alt={name} />
+
+      <span className="name">{name}</span>
+
+      {/*  <button onClick={quantityIncHandler}>inc</button>*/}
+      <span className="quantity">
+        {/* arrows code in html , to represent down arrow , up arrow and x icon  */}
+        <div className="arrow" onClick={quantityDecHandler}>
+          &#10094;
+        </div>
+
+        <span className="value">{quantity}</span>
+        <div className="arrow" onClick={quantityIncHandler}>
+          &#10095;
+        </div>
+      </span>
+      <span className="price">{price}</span>
+      {/*   <button onClick={quantityDecHandler}>dec</button>*/}
+
+      <div className="remove-button">
+        {/* <button onClick={quantityRemoveHandler}>remove</button>*/}
+        <div onClick={quantityRemoveHandler}> &#10005;</div>
       </div>
     </div>
   );
--- src/routes/CheckOutPage/CheckOutPage.jsx
+++ src/routes/CheckOutPage/CheckOutPage.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { CartContext } from "../../contexts/CartContext";
 import CheckOutCardComp from "../../components/CheckOutCardComp/CheckOutCardComp";
-
+import "./checkout.styles.scss";
 function CheckOutPage() {
   const { cartItems } = useContext(CartContext);
 
@@ -10,14 +10,28 @@ function CheckOutPage() {
     total += item.price * item.quantity;
   });
   return (
-    <div>
-      {cartItems.map((item) => (
-        <CheckOutCardComp item={item} />
-      ))}
-      <div>
-        <h1>Total</h1>
-        <h2>{total}</h2>
+    <div className="checkout-container">
+      <div className="checkout-header">
+        <div className="header-block">
+          <span>Product</span>
+        </div>
+        <div className="header-block">
+          <span>Description</span>
+        </div>
+        <div className="header-block">
+          <span>Quantity</span>
+        </div>
+        <div className="header-block">
+          <span>Price</span>
+        </div>
+        <div className="header-block">
+          <span>Remove</span>
+        </div>
       </div>
+      {cartItems.map((item) => (
+        <CheckOutCardComp item={item} key={item.id} />
+      ))}
+      <span className="total">Total : {total}</span>
     </div>
   );
 }
